772885c20a91d349ab95fd058901ef4494967cd7
ferryboy001 <tianci_1080@qq.com>
2023-08-05 23:36:56 +0800

    Initial commit

---
+++ SwiftInboxWeb/Functions/FunctionFile.js
@@ -0,0 +1,16 @@
+﻿Office.initialize = function () {
+}
+
+// 向信息栏添加状态消息的帮助程序函数。
+function statusUpdate(icon, text) {
+  Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+    type: "informationalMessage",
+    icon: icon,
+    message: text,
+    persistent: false
+  });
+}
+
+function defaultStatus(event) {
+  statusUpdate("icon16" , "Hello World!");
+}
